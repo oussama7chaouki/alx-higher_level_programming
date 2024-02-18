e4e2d6f5f9886ef2c601c4fabd7d6689b46b475e
oussama <you@example.com>
2024-02-18 14:02:30 -0800

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT cities.name FROM
-                cities,states where states.id=cities.state_id
+                cities where states.id=cities.state_id
                 AND states.name=%s""", (sys.argv[4],)
     rows = cur.fetchall()
     for row in rows:
