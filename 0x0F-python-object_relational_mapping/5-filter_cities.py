3284b86b3b529db67037aa9b0f91b085a126e90d
oussama <you@example.com>
2024-02-18 14:00:33 -0800

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+    cur.execute("""SELECT cities.name FROM
                 cities,states where states.id=cities.state_id
                 AND states.name=%s""", (sys.argv[4],)
     rows = cur.fetchall()
